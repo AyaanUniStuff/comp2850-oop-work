9a080ccd94b543292831ab36782b10aded57e857
AyaanUniStuff <jfzn5668@leeds.ac.uk>
2025-10-16 13:08:31 +0000

    Quick save until question 3

---
+++ tasks/task2_5_1/test.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var num = 2341
+    println(num)
+    num = 15
+    println(num)
+}
--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,15 @@
 // Task 3.1: command line arguments
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        println("Error: Only two inputs allowed")
+        exitProcess(1)
+    }
+    else {
+        println(args[0])
+        println(args[1])  
+    }
+      
+
+}
